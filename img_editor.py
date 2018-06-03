c110ee6f30a751b55f63b02e27b5458d5c66d975
KostadinovK <kostadinovk2001@gmail.com>
2018-06-04 00:14:01 +0300

    Created the main window

---
+++ img_editor.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  4 00:09:20 2018
+
+@author: Kostadin Kostadinov
+"""
+
+import numpy as np
+import cv2
+
+cv2.namedWindow("ImgEditor v1.0")
+cv2.waitKey(0)
+cv2.destroyAllWindows()
