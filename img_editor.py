e53e7711a8a2c189c00466a0ac56db8de4cf4940
KostadinovK <kostadinovk2001@gmail.com>
2018-06-04 00:36:34 +0300

    Added the UI skeleton

--- img_editor.py
+++ img_editor.py
@@ -8,6 +8,18 @@ Created on Mon Jun  4 00:09:20 2018
 import numpy as np
 import cv2
 
+def dummy(v):
+    pass
+
 cv2.namedWindow("ImgEditor v1.0")
-cv2.waitKey(0)
+cv2.createTrackbar("Contrast","ImgEditor v1.0",1,100,dummy)
+cv2.createTrackbar("Brightness", "ImgEditor v1.0", 50, 100, dummy)
+cv2.createTrackbar("Filter", "ImgEditor v1.0", 0, 1, dummy)
+cv2.createTrackbar("Grayscale","ImgEditor v1.0",0,1, dummy)
+
+while True:
+    k = cv2.waitKey(1) & 0xFF
+    if k == ord('q') or k == ord('Q'):
+        break
+
 cv2.destroyAllWindows()
